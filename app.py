ba19d84d7d71aab3485c169f9bdaa2fae69213f7
Fakhrus Syakir <161460143+KHR00S@users.noreply.github.com>
2024-06-02 16:17:33 +0700

    update

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ import streamlit as st
 import numpy as np
 from PIL import Image
 import onnxruntime as ort
-from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
-import av
+import io
+import cv2
 
 # Load the ONNX model
 onnx_model_path = 'best.onnx'
@@ -40,16 +40,6 @@ def classify_image(image):
     confidence = classes[0][predicted_class_index]
     return predicted_label, confidence
 
-# Video processor for capturing image
-class VideoProcessor(VideoProcessorBase):
-    def __init__(self):
-        self.image = None
-
-    def recv(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-        self.image = Image.fromarray(img)
-        return av.VideoFrame.from_ndarray(img, format="bgr24")
-
 # Main Streamlit app
 def main():
     st.title("Deteksi Kesehatan Gigi")
@@ -62,21 +52,33 @@ def main():
     # Add instructions for the user
     st.write("Silakan pastikan gigi dan gusi Anda terlihat jelas di kamera. Hidung dan kumis harus tidak terlihat (bibir diperbolehkan).")
 
-    # Capture video from camera
-    ctx = webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
-    if ctx.video_processor:
-        if st.button("Ambil Gambar"):
-            image = ctx.video_processor.image
-            if image:
-                st.image(image, caption='Gambar yang Diambil', use_column_width=True)
+    # Button to capture image from camera
+    if st.button("Ambil Gambar"):
+        # OpenCV Capture
+        cap = cv2.VideoCapture(0)
 
-                try:
-                    # Classify the captured image
-                    predicted_label, confidence = classify_image(image)
-                    # Display the prediction
-                    st.write(f"Prediksi: {predicted_label} (Kepercayaan: {confidence:.2f})")
-                except Exception as e:
-                    st.write(f"An error occurred: {e}")
+        # Read image from camera
+        ret, frame = cap.read()
+        
+        if ret:
+            # Convert frame to PIL Image
+            pil_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
+            # Display captured image
+            st.image(pil_image, caption="Gambar yang Diambil", use_column_width=True)
+
+            try:
+                # Classify the captured image
+                predicted_label, confidence = classify_image(pil_image)
+                # Display the prediction
+                st.write(f"Prediksi: {predicted_label} (Kepercayaan: {confidence:.2f})")
+            except Exception as e:
+                st.write(f"An error occurred: {e}")
+        else:
+            st.write("Gagal mengambil gambar dari kamera")
+
+        # Release the camera
+        cap.release()
 
 if __name__ == "__main__":
     main()
